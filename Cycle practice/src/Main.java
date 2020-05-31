f06ad37ef3d91737b1476f33326694a146d8c1ca
giik0n <geek4true@gmail.com>
2020-05-31 20:39:30 +0300

    Update commit

--- Cycle practice/src/Main.java
+++ Cycle practice/src/Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        String name = "Alex";//8 fackts about me
+        byte age = 22;
+        short weight = 93;
+        int height = 182;
+        long birthYear = 1998;
+        float myFavoriteNumber = 7;
+        double myMostHatedNumber = 2;
+        boolean isIKnowJava = true;
+        char myNamesFirstChar = "A".charAt(0);
+
+        System.out.println(String.format("Hi, my name is %s,my age is %s, my weight is %s, my height is %s, i was borned at %s year, my favorite number is %s, my most hated number is %s, you can ask me, do i know java, and i can say %s, as you know my first names char is %s",name,age, weight,height,birthYear,myFavoriteNumber,myMostHatedNumber, isIKnowJava?"Yes":"No",myNamesFirstChar));
+
+
         String[] quialities = {"Alex", "22 years old", "UzNU student", "93 kg",
                 "genius", "billionaire", "playboy", "philanthropist"};
         System.out.print("Hi! ");
 
-        for (int i = 0;i<quialities.length;i++){
+        for (int i = 0;i<quialities.length;i++){//more facts)
             if (i<4) {
                 System.out.println("I'm " + quialities[i]);
             }else if( i==4) {
@@ -13,11 +26,11 @@ public class Main {
             }else{
                 System.out.print(quialities[i]+", ");
             }
-        }
+        }//ok enough fackts
         System.out.println();
-        System.out.println(isPalindrome("abda")?"Its palindrome!":"No, its not:(");
+        System.out.println(isPalindrome("abda")?"Its palindrome!":"No, its not palindrome :(");//Check words "abda" for palindrome
         System.out.println("----------------------Area limited by functions---------------");
-        for (float x = 0; x<=3.14;x+=0.01){
+        for (float x = 0; x<=3.14;x+=0.01){//Here the points which contains in area
             float y = (float) Math.sin(x);
             if (y>= 0.1*2){
                 System.out.println("X: "+ x + ", Y: "+y);
@@ -35,5 +48,5 @@ public class Main {
             j--;
         }
         return true;
-    }
+    }//Function for checking palindrome
 }
--- MoodleLab1/src/Main.java
+++ MoodleLab1/src/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        ArrayList<String> noHarryPotterWords = new ArrayList<>();
+        ArrayList<String> allWords = new ArrayList<>();
+        ArrayList<Integer> distinctHashes = new ArrayList<>();
         String longestWord="";
         int linesWithHarry = 0;
         try {
@@ -14,17 +15,26 @@ public class Main {
                 currentLine = scanner.nextLine();
                 String[] words = currentLine.split(" ");
                 for (int i=0;i<words.length;i++){
-                    if (words[i].length()>longestWord.length()) longestWord = words[i];
-                    if (!words[i].equals("Harry")&&!words[i].equals("Potter")) noHarryPotterWords.add(words[i]);
+                    allWords.add(words[i].replaceAll("[^a-zA-Z ]", ""));//Making list of all words
+                    if (words[i].length()>longestWord.length()) longestWord = words[i];//Finding longest word
+
                 }
-                if (currentLine.contains("Harry")) linesWithHarry++;
+                if (currentLine.contains("Harry")) linesWithHarry++;//Counting "Harry" lines
 
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        for (int i = 0; i < allWords.size(); i++) {
+            if (!distinctHashes.contains(allWords.get(i).hashCode())){
+                distinctHashes.add(allWords.get(i).hashCode());
+            }
+        }
         System.out.println(longestWord);
         System.out.println(linesWithHarry);
+        System.out.println(allWords.size());//Count of all words in text
+        System.out.println(distinctHashes.size());// Count of unique hashCodes
 
 
     }
