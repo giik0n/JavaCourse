6384f29dec4ada07628a59b3ade768de53df5410
giik0n <geek4true@gmail.com>
2020-06-05 14:28:39 +0300

    Update

--- ArrayPractice/src/Main.java
+++ ArrayPractice/src/Main.java
@@ -3,30 +3,48 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+        * @author Alexander Panishchev
+        *Task:
+        * 1.  Find in the net and download a text version of a novel about Harry Potter.  For instance,  "Harry Potter and the Sorcerer's Stone".
+          2. Split the novel into an array of words.
+          3. Clean the words from a punctuation signs.
+          4. Create another array containing distinct words.
+          5. Order the words in alphabetic order.
+        *
+        * */
+
+
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<>();
         ArrayList<String> discintwords = new ArrayList<>();
         try {
+            //----Find in the net and download a text version of a novel about Harry Potter.------
+
             Scanner scanner = new Scanner(new FileInputStream("harryPotterChapterOne.txt"));//1.  Find in the net and download a text version of a novel about Harry Potter.
             String currentLine;
             while (scanner.hasNext()){
                 currentLine = scanner.nextLine();
+                //----Split the novel into an array of words.------------
                 String[] wordsTmp = currentLine.split(" ");
                 for (int i=0;i<wordsTmp.length;i++){
                     if (!wordsTmp.equals(" ")||!wordsTmp.equals("\n"))
+                        //----------Clean the words from a punctuation signs.----------
                         words.add(wordsTmp[i].replaceAll("[^a-zA-Z ]", ""));//2. Split the novel into an array of words. + 3. Clean the words from a punctuation signs.
                 }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < words.size(); i++) {//4. Create another array containing distinct words.
+
+        //------------Create another array containing distinct words----------
+        for (int i = 0; i < words.size(); i++) {
             if (!discintwords.contains(words.get(i))){
                 discintwords.add(words.get(i));
             }
         }
-
+        //---------------------------------------------------------------------
         System.out.println(words);
         words.sort(String::compareToIgnoreCase);//5. Order the words in alphabetic order.
         discintwords.sort(String::compareToIgnoreCase);//5. Order the distinct words in alphabetic order.
--- Cycle practice/src/Main.java
+++ Cycle practice/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Alex";//8 fackts about me
+
+        /*
+        * @author Alexander Panishchev
+        *Task:
+        * 1. Write to console 8 facts about yourself using primitive types. (2 points)
+          2.  Check a string as a palindrome ( 2 points)
+          3.  Get the area limited by the following functions   x = 0 .. 3.14;
+              y = sin(x);
+              y = 0.1 * k, where k is the last cipher in your student card; (2 points)
+        *
+        * */
+//-----------------------8 facts about me------------------------
+        String name = "Alex";
         byte age = 22;
         short weight = 93;
         int height = 182;
@@ -9,44 +21,47 @@ public class Main {
         double myMostHatedNumber = 2;
         boolean isIKnowJava = true;
         char myNamesFirstChar = "A".charAt(0);
+        String aboutMe = String.format("Hi, my name is %s,my age is %s, my weight is %s, my height is %s, i was borned at %s year, my favorite number is %s, my most hated number is %s, you can ask me, do i know java, and i can say %s, as you know my first names char is %s", name, age, weight, height, birthYear, myFavoriteNumber, myMostHatedNumber, isIKnowJava ? "Yes" : "No", myNamesFirstChar);
+        System.out.println(aboutMe);
+        System.out.println("Length: "+aboutMe.length());
 
-        System.out.println(String.format("Hi, my name is %s,my age is %s, my weight is %s, my height is %s, i was borned at %s year, my favorite number is %s, my most hated number is %s, you can ask me, do i know java, and i can say %s, as you know my first names char is %s",name,age, weight,height,birthYear,myFavoriteNumber,myMostHatedNumber, isIKnowJava?"Yes":"No",myNamesFirstChar));
 
 
-        String[] quialities = {"Alex", "22 years old", "UzNU student", "93 kg",
-                "genius", "billionaire", "playboy", "philanthropist"};
-        System.out.print("Hi! ");
+//-------------------------Checking for palindrome-----------------------
+        String sentence = "Hello, olleH";
+        sentence = sentence.replaceAll("[^a-zA-Z ]", "");//Cleaning the sentence from a punctuation signs.
+        System.out.println(isPalindrome(sentence) ? "Its palindrome!" : "No, its not palindrome :(");//Check sentence for palindrome
 
-        for (int i = 0;i<quialities.length;i++){//more facts)
-            if (i<4) {
-                System.out.println("I'm " + quialities[i]);
-            }else if( i==4) {
-                System.out.print("And without suite ");
-                System.out.print("I'm " + quialities[i]+", ");
-            }else{
-                System.out.print(quialities[i]+", ");
-            }
-        }//ok enough fackts
-        System.out.println();
-        System.out.println(isPalindrome("abda")?"Its palindrome!":"No, its not palindrome :(");//Check words "abda" for palindrome
-        System.out.println("----------------------Area limited by functions---------------");
-        for (float x = 0; x<=3.14;x+=0.01){//Here the points which contains in area
-            float y = (float) Math.sin(x);
-            if (y>= 0.1*2){
-                System.out.println("X: "+ x + ", Y: "+y);
-            }
-        }
+
+
+//-------------------------Calculating Area-----------------------
+        System.out.println("Total Area: "+calculatingArea(0.01,0.2));
     }
 
-    static boolean isPalindrome(String str){
+    static boolean isPalindrome(String str){//Function for check palindrome
         int i = 0, j = str.length() - 1;
         while (i < j) {
-
             if (str.charAt(i) != str.charAt(j))
                 return false;
             i++;
             j--;
         }
         return true;
-    }//Function for checking palindrome
+    }
+
+    static double calculatingArea(double deltaX, double y){//function for calculating area by functions
+        double totalArea = 0;
+        double tmpArea;
+
+            for (double x = 0; x < Math.PI; x+=deltaX) {
+                if (Math.sin(x) < y){
+                    tmpArea = Math.sin(x) * deltaX;
+                } else {
+                    tmpArea = y * deltaX;
+                }
+                totalArea += tmpArea;
+            }
+        return totalArea;
+    }
 }
+
